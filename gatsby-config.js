08a5af2d1b691440fe65a5560db7cd0ca54ebebf
Alana Frank <alanamarielle@gmail.com>
2021-01-11 14:29:23 -0500

    added wave component

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,16 @@ module.exports = {
     description:
       'A travel blog showcasing past trips, future plans, and what we are doing in the meantime.',
   },
-  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js'),
+        },
+      },
+    },
+  ],
 };
